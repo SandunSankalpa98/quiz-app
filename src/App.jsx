f5212b9ec026d2741d9241d47202fd1625f5679c
A.G.Sandun Sankalpa <sandunsankalpa10@gmail.com>
2024-08-05 17:30:42 +0530

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './Components/Header'
+import Main from './Components/Main'
+
+export default function App() {
+  return (
+    <div className='app'>
+      <Header/>
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  )
+}
