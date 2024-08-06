48a46a16520cd9a6611dd3c735b87b18377eed76
A.G.Sandun Sankalpa <sandunsankalpa10@gmail.com>
2024-08-06 12:28:32 +0530

    create reducer hooks for fetching qustions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
 
+import { useEffect, useReducer } from 'react'
 import Header from './Components/Header'
 import Main from './Components/Main'
+import Loader from './Components/Loader';
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'active', 'finished'
+  status: "loading"
+}
+function reducer(state, action){
+  switch(action.type){
+    case 'dataReceived':
+      return{
+        ...state,
+        questions:action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return{
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unkonwn")
+  }
+}
 
 export default function App() {
+
+  const [{questions, status}, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:9000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
+  
   return (
     <div className='app'>
       <Header/>
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader/>}
       </Main>
     </div>
   )
